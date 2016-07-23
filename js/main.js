a747a0cf48dbfb31b043ee49f46041abc3fd7013
zeligzhou <zeligzhou@gmail.com>
2016-07-23 15:04:10 +0800

    any motion

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+	
+});
+
+/* Animation */
+$("#opacity").on("tap",function(){
+    $("#opacityShow .obj").toggleClass('show');
+})
+$("#sizea").on("tap",function(){
+    $("#sizeaShow .obj").toggleClass('show');
+})
+$("#sizeb").on("tap",function(){
+    $("#sizebShow .obj").toggleClass('show');
+})
+$("#move").on("tap",function(){
+    $("#moveShow .obj").toggleClass('show');
+})
