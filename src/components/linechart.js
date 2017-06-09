13a7733bff6a4a087210dc4d44aea42e9071bb91
zhouzhou <zhouzhou06@maoyan.com>
2017-06-09 15:22:34 +0800

    line chart

--- src/components/linechart.js
+++ src/components/linechart.js
@@ -8,14 +8,44 @@ class Linechart extends Component {
   componentDidMount () {
     var data = lineChartData;
     console.table(data.box);
-    var svg = d3.select("#svg").attr("height","200").attr("width","90%");
+    var svg = d3.select("#svg").attr("height","200").attr("width","96%");
     var _data = tool.objConverter(data.box,["label","week","real","forecast","date"]);
+    tool.objSumGroups(_data,"total");
+
     var myChart = new dimple.chart(svg,_data);
+
     console.table(_data);
-    var x = myChart.addCategoryAxis("x","label");
-    myChart.addMeasureAxis("y","real");
-    myChart.addSeries(null,dimple.plot.line)
-    myChart.draw();
+    // Set a variety of default colors
+    // myChart.defaultColors = [
+    // 	new dimple.color("#FF0000", "Blue"), // Set a red fill with a blue stroke
+    // 	new dimple.color("#00FF00"), // Set a green fill with a darker green stroke
+    // 	new dimple.color("rgb(0, 0, 255)") // Set a blue fill with a darker blue stroke
+    // ];
+    var x = myChart.addTimeAxis("x","date", "%Y-%m-%d", "%m/%d");
+    x.title = null;
+    x.timePeriod = d3.timeDay;
+    x.timeInterval = 2
+    x.fontSize = 8;
+
+    var y1 = myChart.addMeasureAxis("y","total");
+    y1.fontSize = 8;
+    y1.title = null;
+    // var y2 = myChart.addMeasureAxis("y","forecast");
+    // y2.fontSize = 8;
+    // y2.title = null;
+    // //y2.hidden = true;
+    // y2.stacked = true;
+    //y2.colors = ["#DA9694", "#FABF8F", "#C4D79B"];
+    var s1 = myChart.addSeries("type",dimple.plot.line,[x,y1]);
+    //myChart.assignColor(new Date("2017-06-05"), "green", "black", 1);
+    //var s2 = myChart.addSeries(null,dimple.plot.area,[x,y2]);
+    s1.getTooltipText = function (e) {
+      console.log(e);
+        return [
+            e.cx + " " + e.cy + "万 分账票房"
+        ];
+    };
+    myChart.draw(200);
 
   }
   render() {
--- src/lib/tool.js
+++ src/lib/tool.js
@@ -7,11 +7,35 @@ const tool = {
     for(let i=0;i<lengthOfRet;i++){
       let singleTon = {};
       for(let j=0;j<lengthOfKeys;j++){
-        singleTon[keysOfObj[j]] = obj[keysOfObj[j]][i] || 0;
+        singleTon[keysOfObj[j]] = ((obj[keysOfObj[j]][i] === "--")?0:obj[keysOfObj[j]][i]) || 0;
       }
       ret.push(singleTon);
     }
     return ret;
+  },
+  objSumGroups:(obj,key)=>{
+    let today = 999;
+    let todayData = null;
+    for (let j = 0; j < obj.length; j++) {
+        obj[j][key] = 0;
+
+        //for (let i = 0; i < groups.length; i++) {
+          if(j > today){
+            obj[j]['type'] = "forecast";
+            obj[j][key] = obj[j]['forecast']*1.0;
+          }else{
+            obj[j]['type'] = "real";
+            obj[j][key] = obj[j]['real']*1.0
+          }
+          if(obj[j]['week'] === "今天"){
+            todayData = Object.assign({},obj[j]);
+            todayData.type = "forecast";
+            //todayData.total = 0;
+            today = j;
+          }
+        //}
+      }
+      obj.push(todayData);
   }
 }
 export default tool
